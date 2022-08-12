97c0dce51c43f0e5230b8c48afbd4b41ce851b24
Filipe Sousa <filpess@hotmail.com>
2022-08-12 14:01:21 -0300

    Atualização dos pacotes e estilização do html

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/main.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ],
+  },
+};
